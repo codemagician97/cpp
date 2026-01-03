1d73ce949293de319eaec2e025005b6b6d892011
cmt <codemagician97@163.com>
2026-01-03 09:51:18 +0800

    实现sort函数：冒泡排序

--- main.cpp
+++ main.cpp
@@ -3,7 +3,26 @@ using namespace std;
 
 void sort(int arr[], int size)
 {
+	int tmp = 0;
+	for(int i = 0; i < size - 1; ++i)
+	{
+		for(int j = 0; j < size - 1; j++)
+		{
+			
+			if(arr[j] > arr[j+1])
+			{	
+				tmp = arr[j];
+				arr[j] = arr[i];
+				arr[i] = tmp;
+			}
+		}
+	}
+}
 
+void printArray(int arr[], int size)
+{
+	for(int i = 0; i < size - 1; i++)
+		cout << arr[i] << endl;
 }
 
 int main()
@@ -11,6 +30,8 @@ int main()
 	int arr[] = { 12, 4, 89, 43, 21, 78 };
 	int size = sizeof(arr) / sizeof(arr[0]);
 	sort(arr, size);
+
+	printArray(arr, size);
 
 	return 0;
 }
